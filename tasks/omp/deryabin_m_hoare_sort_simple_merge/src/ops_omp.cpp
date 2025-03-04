d510194136ed397ff7a0ea5bd89d4dc3cd5cb7c0
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-03-04 07:36:42 +0300

    Update ops_omp.cpp

--- tasks/omp/deryabin_m_hoare_sort_simple_merge/src/ops_omp.cpp
+++ tasks/omp/deryabin_m_hoare_sort_simple_merge/src/ops_omp.cpp
@@ -127,8 +127,8 @@ bool deryabin_m_hoare_sort_simple_merge_omp::HoareSortTaskOpenMP::RunImpl() {
       {
         for (short j = 0; j < chunk_count; j++) {
           MergeTwoParts(input_array_A_, j * (short)min_chunk_size_ << ((short)i + 1),
-                       ((j + 1) * (short)min_chunk_size_ << ((short)i + 1)) - 1, dimension_);
-          #pragma omp atomic
+                        ((j + 1) * (short)min_chunk_size_ << ((short)i + 1)) - 1, dimension_);
+#pragma omp atomic
           chunk_count--;
         }
       }
