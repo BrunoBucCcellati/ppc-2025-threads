bd20fe7f2917c44c424aad3c1aa8e7ba50a71d24
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-03-04 03:24:59 +0300

    Update ops_omp.hpp

--- tasks/omp/deryabin_m_hoare_sort_simple_merge/include/ops_omp.hpp
+++ tasks/omp/deryabin_m_hoare_sort_simple_merge/include/ops_omp.hpp
@@ -8,6 +8,11 @@
 
 namespace deryabin_m_hoare_sort_simple_merge_omp {
 
+ // сортировка Хоара
+ void HoaraSort(std::vector<double>& a, size_t first, size_t last);
+ // слияние двух отсортированных частей
+ void MergeTwoParts(std::vector<double>& a, size_t left, size_t right) const;
+
 class HoareSortTaskSequential : public ppc::core::Task {
  public:
   explicit HoareSortTaskSequential(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
@@ -17,11 +22,6 @@ class HoareSortTaskSequential : public ppc::core::Task {
   bool PostProcessingImpl() override;
 
  private:
-  // сортировка Хоара
-  void HoaraSort(std::vector<double>& a, size_t first, size_t last);
-  // слияние двух отсортированных частей
-  void MergeTwoParts(std::vector<double>& a, size_t left, size_t right) const;
-
   std::vector<double> input_array_A_;  // входной массив
   size_t dimension_;                   // его размер
   size_t min_chunk_size_;  // размер частей на которые будет разбиваться исходный массив
@@ -36,11 +36,6 @@ class HoareSortTaskOpenMP : public ppc::core::Task {
   bool PostProcessingImpl() override;
 
  private:
-  // сортировка Хоара
-  void HoaraSort(std::vector<double>& a, size_t first, size_t last);
-  // слияние двух отсортированных частей
-  void MergeTwoParts(std::vector<double>& a, size_t left, size_t right) const;
-
   std::vector<double> input_array_A_;  // входной массив
   size_t dimension_;                   // его размер
   size_t min_chunk_size_;  // размер частей на которые будет разбиваться исходный массив
