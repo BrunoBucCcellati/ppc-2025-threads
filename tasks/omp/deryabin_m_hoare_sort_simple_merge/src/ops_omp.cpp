c68cb0bf271386fc33cfbf51a2ea99bc20286b5f
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-03-04 07:34:28 +0300

    Update ops_omp.cpp

--- tasks/omp/deryabin_m_hoare_sort_simple_merge/src/ops_omp.cpp
+++ tasks/omp/deryabin_m_hoare_sort_simple_merge/src/ops_omp.cpp
@@ -121,10 +121,17 @@ bool deryabin_m_hoare_sort_simple_merge_omp::HoareSortTaskOpenMP::RunImpl() {
   }
 #pragma omp barrier
   for (size_t i = 0; i < (size_t)(log((double)chunk_count_) / std::numbers::ln2); i++) {
-    for (short j = 0; j < chunk_count; j++) {
-      MergeTwoParts(input_array_A_, j * (short)min_chunk_size_ << ((short)i + 1),
-                    ((j + 1) * (short)min_chunk_size_ << ((short)i + 1)) - 1, dimension_);
-      chunk_count--;
+#pragma omp parallel default(none)
+    {
+#pragma omp critical
+      {
+        for (short j = 0; j < chunk_count; j++) {
+          MergeTwoParts(input_array_A_, j * (short)min_chunk_size_ << ((short)i + 1),
+                       ((j + 1) * (short)min_chunk_size_ << ((short)i + 1)) - 1, dimension_);
+          #pragma omp atomic
+          chunk_count--;
+        }
+      }
     }
   }
   return true;
