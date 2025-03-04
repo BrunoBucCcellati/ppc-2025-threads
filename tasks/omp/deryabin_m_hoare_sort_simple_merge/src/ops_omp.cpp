233f69e40b1127685336074585418f1cc109decd
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-03-04 05:21:00 +0300

    Update ops_omp.cpp

--- tasks/omp/deryabin_m_hoare_sort_simple_merge/src/ops_omp.cpp
+++ tasks/omp/deryabin_m_hoare_sort_simple_merge/src/ops_omp.cpp
@@ -114,17 +114,17 @@ bool deryabin_m_hoare_sort_simple_merge_omp::HoareSortTaskOpenMP::ValidationImpl
 }
 
 bool deryabin_m_hoare_sort_simple_merge_omp::HoareSortTaskOpenMP::RunImpl() {
-  size_t chunk_count = chunk_count_;
+  short chunk_count = (short)chunk_count_;
 #pragma omp parallel
 #pragma omp for
-  for (size_t count = 0; count < chunk_count_; count++) {
-    HoaraSort(input_array_A_, count * min_chunk_size_, ((count + 1) * min_chunk_size_) - 1);
+  for (short count = 0; count < chunk_count; count++) {
+    HoaraSort(input_array_A_, count * (short)min_chunk_size_, ((count + 1) * (short)min_chunk_size_) - 1);
   }
 #pragma omp barrier
   for (size_t i = 0; i < (size_t)(log((double)chunk_count_) / std::numbers::ln2); i++) {
 #pragma omp for
-    for (size_t j = 0; j < chunk_count; j++) {
-      MergeTwoParts(input_array_A_, j * min_chunk_size_ << (i + 1), ((j + 1) * min_chunk_size_ << (i + 1)) - 1,
+    for (short j = 0; j < chunk_count; j++) {
+      MergeTwoParts(input_array_A_, j * (short)min_chunk_size_ << ((short)i + 1), ((j + 1) * (short)min_chunk_size_ << ((short)i + 1)) - 1,
                     dimension_);
       chunk_count--;
     }
