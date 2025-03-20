f63ee8e57d287dadf41cbb3089d65e5d6c13e842
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-03-21 00:46:00 +0300

    Update main.cpp

--- tasks/omp/deryabin_m_hoare_sort_simple_merge/func_tests/main.cpp
+++ tasks/omp/deryabin_m_hoare_sort_simple_merge/func_tests/main.cpp
@@ -197,6 +197,36 @@ TEST(deryabin_m_hoare_sort_simple_merge_omp, test_random_array_large_pieces) {
   ASSERT_EQ(true_solution, out_array[0]);
 }
 
+TEST(deryabin_m_hoare_sort_simple_merge_omp, test_random_array_large_pieces_reversed_order) {
+  // Create data
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::vector<double> input_array(800);
+  std::ranges::generate(input_array.begin(), input_array.end(), [value = 800]() mutable { return value--; });
+  std::vector<std::vector<double>> in_array(1, input_array);
+  size_t chunk_count = 2;
+  std::vector<double> output_array(800);
+  std::vector<std::vector<double>> out_array(1, output_array);
+  std::vector<double> true_solution(input_array);
+  std::ranges::sort(true_solution.begin(), true_solution.end());
+
+  // Create TaskData
+  auto task_data_omp = std::make_shared<ppc::core::TaskData>();
+  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t*>(in_array.data()));
+  task_data_omp->inputs_count.emplace_back(input_array.size());
+  task_data_omp->inputs_count.emplace_back(chunk_count);
+  task_data_omp->outputs.emplace_back(reinterpret_cast<uint8_t*>(out_array.data()));
+  task_data_omp->outputs_count.emplace_back(output_array.size());
+
+  // Create Task
+  deryabin_m_hoare_sort_simple_merge_omp::HoareSortTaskOpenMP hoare_sort_task_openmp(task_data_omp);
+  ASSERT_EQ(hoare_sort_task_openmp.Validation(), true);
+  hoare_sort_task_openmp.PreProcessing();
+  hoare_sort_task_openmp.Run();
+  hoare_sort_task_openmp.PostProcessing();
+  ASSERT_EQ(true_solution, out_array[0]);
+}
+
 TEST(deryabin_m_hoare_sort_simple_merge_omp, test_partially_sorted_array) {
   // Create data
   std::random_device rd;
