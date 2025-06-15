fdc5f96e97398040a9f26039d46360123c3602cf
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-06-15 17:54:38 +0300

    Update main.cpp

--- tasks/seq/deryabin_m_hoare_sort_simple_merge/perf_tests/main.cpp
+++ tasks/seq/deryabin_m_hoare_sort_simple_merge/perf_tests/main.cpp
@@ -23,7 +23,7 @@ TEST(deryabin_m_hoare_sort_simple_merge_seq, test_pipeline_run_Seq) {
   std::shuffle(input_array.begin(), input_array.end(), gen);
   std::vector<std::vector<double>> in_array(1, input_array);
   size_t chunk_count = 256;
-  std::vector<double> output_array(64000);
+  std::vector<double> output_array(512000);
   std::vector<std::vector<double>> out_array(1, output_array);
   std::vector<double> true_solution(input_array);
   std::ranges::sort(true_solution.begin(), true_solution.end());
@@ -65,7 +65,7 @@ TEST(deryabin_m_hoare_sort_simple_merge_seq, test_task_run_Seq) {
   std::shuffle(input_array.begin(), input_array.end(), gen);
   std::vector<std::vector<double>> in_array(1, input_array);
   size_t chunk_count = 256;
-  std::vector<double> output_array(64000);
+  std::vector<double> output_array(512000);
   std::vector<std::vector<double>> out_array(1, output_array);
   std::vector<double> true_solution(input_array);
   std::ranges::sort(true_solution.begin(), true_solution.end());
